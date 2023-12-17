cfc6472e0bbb5645015f4518cd9cb78b8f8706e6
nadia-amz <nadiaamouz@gmail.com>
2023-12-17 18:14:30 +0100

    commit14

--- script.js
+++ script.js
@@ -43,70 +43,139 @@
 
     let signinBtn = document.getElementById("signinBtn");
     let signupBtn = document.getElementById("signupBtn");
+    let RegisterBtn = document.getElementById("RegisterBtn").addEventListener("click", function(event) {
+        event.preventDefault();});
     let field = document.getElementById("field");
-    let title = document.getElementById("form-title");
+    let passwordField = document.getElementById("passwordField");
+    let title = document.getElementById("title");
 
-    signinBtn.onclick = function(){
+    signinBtn.onclick = function(e){
+        e.preventDefault();
         field.style.maxHeight = "0";
+        field.style.visibility = "hidden";
+        passwordField.style.maxHeight = "0";
+        passwordField.style.visibility = "hidden";
         title.innerHTML = "Sign In";
         signupBtn.classList.add("disable");
         signinBtn.classList.remove("disable");
     }
 
-    signupBtn.onclick = function(){
-        field.style.maxHeight = "65px";
+    signupBtn.onclick = function(e){
+        e.preventDefault();
+        field.style.maxHeight = "60px";
+        field.style.visibility = "visible";
+        passwordField.style.maxHeight = "60px";
+        passwordField.style.visibility = "visible";
         title.innerHTML = "Sign Up";
         signupBtn.classList.remove("disable");
         signinBtn.classList.add("disable");
     }
 
-    const form = document.getElementById("form");
-    const username = document.getElementById("contact-name");
-    const email = document.getElementById("contact-email");
-    const password = document.getElementById("contact-password");
+    // const form = document.getElementById("form");
 
-    signinBtn.addEventListener('click', (e) => {
-        console.log('Sign In button clicked');
-        e.preventDefault(); 
-        checkInputs();
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("password2");
 
-    });
 
-    signupBtn.addEventListener('click', (e) => {
-        console.log('Sign up button clicked');
-        e.preventDefault(); 
-        checkInputs();
+    // username.addEventListener("focus", () => {
+    //     checkInput(username);
+    // });
+    
+    // email.addEventListener("focus", () => {
+    //     checkInput(email);
+    // });
+    
+    // password.addEventListener("focus", () => {
+    //     checkInput(password);
+    // });
 
-    });
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        checkInputs();
-    })
+    // form.addEventListener("submit", (e) => {
+    //     e.preventDefault();
+    //     checkInputs();
+    // })
 
         function checkInputs(){
+            // checkInput(username);
+            // checkInput(email);
+            // checkInput(password);
             const usernameValue = username.value.trim();
             const emailValue = email.value.trim();
             const passwordValue = password.value.trim();
+            const confirmPasswordValue = confirmPassword.value.trim();
 
-            if(usernameValue ===''){
-                setErrorFor(username, 'Username cannot be blank');
+            const isUsernameVisible = parseInt(getComputedStyle(field).maxHeight) > 0;
+            const isConfirmPasswordVisible = parseInt(getComputedStyle(passwordField).maxHeight) > 0;
+
+
+              if (isUsernameVisible) {
+                 if (usernameValue === '') {
+                    setErrorFor(username, 'Username cannot be blank');
+            } else {
+            setSuccessFor(username);
+            }
+            }
+            if(emailValue === ''){
+                setErrorFor(email, 'Email cannot be blank');
+            }else if(!isEmail(emailValue)){
+                setErrorFor(email, 'Email is not valid');
             }else{
-                setSuccessFor(username);
+                setSuccessFor(email);
             }
 
+            if(passwordValue ===''){
+                setErrorFor(password, 'Password cannot be blank');
+            }else if(!isPassword(passwordValue)){
+                setErrorFor(password, 'Password is not valid');
+            }else{
+                setSuccessFor(password);
+            }
+            if (isConfirmPasswordVisible) {
+            if(confirmPasswordValue ===''){
+                setErrorFor(confirmPassword, 'Confirm password cannot be blank');
+            }else if(passwordValue !== confirmPasswordValue){
+                setErrorFor(confirmPassword, 'Passwords does not match');
+            }else {
+                setSuccessFor(confirmPassword);
+            }
+             }
         }
+
+
+
+        // function checkInput(input) {
+        //     const inputValue = input.value.trim();
+        //     const formControl = input.parentElement;
+        //     const span = formControl.querySelector('span');
+        
+        //     if (document.activeElement === input && inputValue === '') {
+        //         setErrorFor(input, '');
+        //         span.style.visibility = 'visible'; 
+        //     } else {
+        //         setSuccessFor(input);
+        //         span.style.visibility = 'hidden'; 
+        //     }
+        // }
+        
 
         function setErrorFor(input, message){
             const formControl = input.parentElement;
             const span = formControl.querySelector('span');
 
             span.innerText = message;
-            formControl.className = '-in error';
+            formControl.className = 'in error';
         }
 
         function setSuccessFor(input){
             const formControl = input.parentElement;
-            formControl.className = '-in success';
+            formControl.className = 'in success';
+        }
+
+        function isEmail(email){
+            return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+        }
+
+        function isPassword(password){
+            return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
         }
